8873902d9153ee9183710400658bb197f951a16f
cayman718 <a44362652@gmail.com>
2024-11-26 15:36:36 +0800

    物件教學

--- test.php
+++ test.php
@@ -6,35 +6,31 @@ class DB
     protected $pdo;
     protected $table;
 
-
     function __construct($table)
     {
         $this->table = $table;
         $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
-
     // 撈出全部資料
-    // 1.整張資料表
-    // 2.有條件
-    // 3.其他sql功能
     function all(...$arg)
     {
         $sql = "SELECT * FROM $this->table";
-        if (!empty($args[0])) {
-            if (is_array($arg[0])) {
 
+        if (!empty($arg[0])) {  // 使用 $arg 代替 $args
+            if (is_array($arg[0])) {
                 $where = $this->a2s($arg[0]);
-                $sql = $sql . "WHERE " . join(" && ", $where);
+                $sql = $sql . " WHERE " . join(" && ", $where);  // 加上空格
             } else {
-                // $sql =$sql. $arg[0];
                 $sql .= $arg[0];
             }
         }
+
         if (!empty($arg[1])) {
-            $sql = $sql . $arg[1];
+            $sql = $sql . " " . $arg[1];  // 加上空格
         }
-        return $this->fetchAll($sql);
+
+        return $this->fetchAll($sql);  // 修正為 fetchAll
     }
 
     function find($id)
@@ -43,49 +39,28 @@ class DB
 
         if (is_array($id)) {
             $where = $this->a2s($id);
-            $sql = $sql . " WHERE " . join(" && ", $where);
+            $sql = $sql . " WHERE " . join(" && ", $where);  // 加上空格
         } else {
-            // $sql =$sql. $arg[0];
             $sql .= " WHERE `id`='$id'";
         }
-        return $this->fetchAll($sql);
+        return $this->fetchAll($sql);  // 修正為 fetchAll
     }
-
-    function del($id)
-    {
-        $sql = "DELETE * FROM $this->table";
-
-        if (is_array($id)) {
-            $where = $this->a2s($id);
-            $sql = $sql . "WHERE " . join(" && ", $where);
-        } else {
-            // $sql =$sql. $arg[0];
-            $sql .= "WHERE `id`='$id'";
-        }
-        return $this->pdo->exec($sql);
-    }
-
-
-
-
-
-
 
     // 把陣列轉成條件字串陣列
     function a2s($array)
     {
         $tmp = [];
         foreach ($array as $key => $value) {
-            $tmp = "`$key`='$value'";
+            $tmp[] = "`$key`='$value'";  // 正確加入條件到數組
         }
         return $tmp;
     }
-
 
     function fetchOne($sql)
     {
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
+
     function fetchAll($sql)
     {
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
@@ -97,8 +72,6 @@ class DB
 //     return $this->pdo->query($sql)->fetchAll();
 // }
 
-
-
 function dd($array)
 {
     echo "<pre>";
@@ -109,6 +82,6 @@ function dd($array)
 $DEPT = new DB('dept');
 // $dept = $DEPT->q("SELECT * FROM dept");
 // $dept = $DEPT->all(['id' => 3]);
-$dept = $DEPT->all("Order by `id` DESC");
+$dept = $DEPT->all(" ORDER BY `id` DESC");
 $dept = $DEPT->find(['code' => '404']);
 dd($dept);
